b536ddc7ab2b058d159c06bc195c95ce75d0f222
Niku <nokka200@gmail.com>
2024-07-11 17:24:05 +0300

    4.4. aloitettu

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -2,9 +2,24 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => { 
+  test('of empty list is zero', () => { 
+
+  })
+
+  test('when list has only one blog equals the likes of that', () => { 
+
+  })
+
+  test('of a bigger list is calculated right', () => { 
+    
+  })
 })
