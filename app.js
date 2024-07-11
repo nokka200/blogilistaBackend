f18482fef6e323a37c6f334fae7b2f0ecd9a9856
Niku <nokka200@gmail.com>
2024-07-11 13:24:15 +0300

    täytetty app.js

--- app.js
+++ app.js
@@ -2,5 +2,20 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const notesRouter = require('./controllers/blogs')
+const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+.then(() => {
+  console.log('connected to MongoDB')
+})
+.catch((error) => {
+  console.log('error connection to MongoDB:', error.message)
+})
+
+app.use('/api/notes', blogRouter)
+
+module.exports = app
