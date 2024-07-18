9053c0134b956816de06e02dcfc0e3722a259af4
Niku <nokka200@gmail.com>
2024-07-18 11:26:46 +0300

    Tehtävä 4.18 valmis

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -21,5 +22,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
