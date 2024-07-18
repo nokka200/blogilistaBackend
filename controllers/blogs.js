dfb5a4b384de477399a301e991cc0bdd7db94463
Niku <nokka200@gmail.com>
2024-07-18 10:51:22 +0300

    Aloitettu 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,7 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
 
 blogRouter.get('/', async (request, response) => {
   const blog = await Blog.find({})
@@ -9,15 +11,21 @@ blogRouter.get('/', async (request, response) => {
 blogRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
     author: body. author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+  
+  response.json(savedBlog)
 })
 
 blogRouter.delete('/:id', async (request, response) => { 
